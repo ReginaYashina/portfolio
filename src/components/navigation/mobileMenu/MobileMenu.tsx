be13b9905444e5773d67797142e3b34117800401
Regina Yashina <reginayashina@inbox.ru>
2024-08-06 21:27:45 +0300

    feat: add useState for mobile menu

--- src/components/navigation/mobileMenu/MobileMenu.tsx
+++ src/components/navigation/mobileMenu/MobileMenu.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Menu } from "../menu/Menu";
 import { S } from "../Menu_Styles";
 
@@ -9,12 +9,19 @@ type MobileMenuPropsType = {
 export const MobileMenu: React.FC<MobileMenuPropsType> = (
   props: MobileMenuPropsType
 ) => {
+  const [menuIsOpen, setmenuIsOpen] = useState(false);
+  const onBurgerBtnClick = () => {
+    setmenuIsOpen(!menuIsOpen);
+  };
   return (
     <S.MobileMenu>
-      <S.BurgerButton isOpen={false}>
+      <S.BurgerButton isOpen={menuIsOpen} onClick={onBurgerBtnClick}>
         <span></span>
       </S.BurgerButton>
-      <S.MobileMenuWrapper isOpen={false}>
+      <S.MobileMenuWrapper
+        isOpen={menuIsOpen}
+        onClick={() => setmenuIsOpen(false)}
+      >
         <Menu menuItems={props.menuItems} />
       </S.MobileMenuWrapper>
     </S.MobileMenu>
